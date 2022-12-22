fab73165c190c7225d425d0c86159179427073ca
hafsayildiz <hafsayildiz54@gmail.com>
2022-12-22 23:03:35 +0300

    sayfaları staticleştirme

--- activity/models.py
+++ activity/models.py
@@ -41,7 +41,6 @@ class activity(models.Model):
     slug = models.SlugField(null=False, unique=True)
     parent = ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
